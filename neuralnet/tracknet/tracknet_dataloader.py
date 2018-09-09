b5e54978ba92e455e03931fb26f740b96c085e89
Saeid <saeid_motevali@yahoo.com>
2018-09-09 18:15:54 -0400

    first commit

---
+++ neuralnet/tracknet/tracknet_dataloader.py
@@ -0,0 +1,65 @@
+import os
+import random
+from random import shuffle
+
+import numpy as np
+import torch
+
+import utils.img_utils as imgutils
+from neuralnet.datagen import Generator
+import math
+from commons.MAT import Mat
+
+sep = os.sep
+
+
+class PatchesGenerator(Generator):
+    def __init__(self, **kwargs):
+        super(PatchesGenerator, self).__init__(**kwargs)
+        self.patch_shape = self.run_conf.get('Params').get('patch_shape')
+        self.patch_pad = self.run_conf.get('Params').get('patch_pad')
+        self.patch_offset = self.run_conf.get('Params').get('patch_offset')
+        self.k_half = int(math.floor(self.patch_shape[0] / 2))
+        self._load_indices()
+        print('Patches:', self.__len__())
+
+    def _load_indices(self):
+        for ID, img_file in enumerate(self.images):
+            mat_file = Mat(mat_file=self.images_dir + sep + img_file)
+
+            V = mat_file.get_graph('V')
+            A = mat_file.get_graph('A')
+            I = mat_file.get_image('I')
+            self.image_objects[ID] = I[:, :, 1]
+
+            path_index = mat_file.get_graph('pathNode')
+            vessel_pathidx = np.where(path_index)[0]
+            u_pos = V[vessel_pathidx, :]
+            A = A
+            b = np.where(A[vessel_pathidx, :])[0]
+            b_pos = V[b, :]
+            for (i, j), output in zip(u_pos, b_pos - u_pos):
+                row_from, row_to = i - self.k_half, i + self.k_half + 1
+                col_from, col_to = j - self.k_half, j + self.k_half + 1
+                if row_from < 0 or col_from < 0:
+                    continue
+                if row_to >= self.image_objects[ID].shape[0] or col_to >= self.image_objects[ID].shape[1]:
+                    continue
+
+                self.indices.append([ID, [i, j], output.tolist()])
+
+    def __getitem__(self, index):
+        ID, (i, j), out = self.indices[index]
+        row_from, row_to = i - self.k_half, i + self.k_half + 1
+        col_from, col_to = j - self.k_half, j + self.k_half + 1
+
+        row_from = int(row_from)
+        row_to = int(row_to)
+        col_from = int(col_from)
+        col_to = int(col_to)
+        img_tensor = self.image_objects[ID][row_from:row_to, col_from:col_to][..., None]
+
+        if self.transforms is not None:
+            img_tensor = self.transforms(img_tensor)
+
+        return {'IDs': ID, 'POS': np.array([i, j]), 'inputs': np.array(img_tensor), 'labels': torch.FloatTensor(out)}
---
+++ neuralnet/tracknet/tracknet_trainer.py
@@ -0,0 +1,107 @@
+import os
+import sys
+
+import numpy as np
+import torch
+from PIL import Image as IMG
+
+import utils.img_utils as imgutils
+from neuralnet.torchtrainer import NNTrainer
+from neuralnet.utils.measurements import ScoreAccumulator
+
+sep = os.sep
+
+
+class TracknetTrainer(NNTrainer):
+    def __init__(self, **kwargs):
+        NNTrainer.__init__(self, **kwargs)
+        self.patch_shape = self.run_conf.get('Params').get('patch_shape')
+        self.patch_offset = self.run_conf.get('Params').get('patch_offset')
+
+
+    def train(self, optimizer=None, data_loader=None, validation_loader=None):
+
+        if validation_loader is None:
+            raise ValueError('Please provide validation loader.')
+
+        logger = NNTrainer.get_logger(self.log_file)
+        print('Training...')
+        for epoch in range(0, self.epochs):
+            self.model.train()
+            score_acc = ScoreAccumulator()
+            running_loss = 0.0
+            self.adjust_learning_rate(optimizer=optimizer, epoch=epoch + 1)
+            for i, data in enumerate(data_loader, 0):
+                inputs, labels = data['inputs'].to(self.device), data['labels'].to(self.device)
+
+                optimizer.zero_grad()
+                outputs = self.model(inputs)
+                _, predicted = torch.max(outputs, 1)
+
+                loss = torch.dist(outputs, labels, p=1)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += float(loss.item())
+                current_loss = loss.item()
+                p, r, f1, a = score_acc.reset().add_tensor(labels, predicted).get_prf1a()
+                if (i + 1) % self.log_frequency == 0:  # Inspect the loss of every log_frequency batches
+                    current_loss = running_loss / self.log_frequency if (i + 1) % self.log_frequency == 0 \
+                        else (i + 1) % self.log_frequency
+                    running_loss = 0.0
+
+                self.flush(logger, ','.join(str(x) for x in [0, 0, epoch + 1, i + 1, p, r, f1, a, current_loss]))
+                print('Epochs[%d/%d] Batch[%d/%d] loss:%.5f pre:%.3f rec:%.3f f1:%.3f acc:%.3f' %
+                      (epoch + 1, self.epochs, i + 1, data_loader.__len__(), current_loss, p, r, f1, a),
+                      end='\r' if running_loss > 0 else '\n')
+
+            self.checkpoint['epochs'] += 1
+            if (epoch + 1) % self.validation_frequency == 0:
+                self.evaluate(data_loaders=validation_loader, force_checkpoint=self.force_checkpoint, logger=logger,
+                              mode='train')
+        try:
+            logger.close()
+        except IOError:
+            pass
+
+    def evaluate(self, data_loaders=None, force_checkpoint=False, logger=None, mode=None):
+        assert (logger is not None), 'Please Provide a logger'
+        self.model.eval()
+
+        print('\nEvaluating...')
+        with torch.no_grad():
+            all_score = ScoreAccumulator()
+
+            for loader in data_loaders:
+                current_score = ScoreAccumulator()
+                img_obj = loader.dataset.image_objects[0]
+                segmented_map, labels_acc = [], []
+
+                for i, data in enumerate(loader, 0):
+                    inputs, labels = data['inputs'].to(self.device), data['labels'].to(self.device)
+                    outputs = self.model(inputs)
+                    _, predicted = torch.max(outputs, 1)
+
+                    current_score.add_tensor(labels, predicted)
+                    all_score.accumulate(current_score)
+                    if mode is 'test':
+                        segmented_map += outputs.clone().cpu().numpy().tolist()
+                        labels_acc += labels.clone().cpu().numpy().tolist()
+
+                self.flush(logger, ','.join(
+                    str(x) for x in [img_obj.file_name, 1, self.checkpoint['epochs'], 0] + current_score.get_prf1a()))
+
+                if mode is 'test':
+                    segmented_map = np.exp(np.array(segmented_map)[:, 1, :, :]).squeeze()
+                    segmented_map = np.array(segmented_map * 255, dtype=np.uint8)
+                    # labels_acc = np.array(np.array(labels_acc).squeeze()*255, dtype=np.uint8)
+
+                    maps_img = imgutils.merge_patches(patches=segmented_map, image_size=img_obj.working_arr.shape,
+                                                      patch_size=self.patch_shape,
+                                                      offset_row_col=self.patch_offset)
+                    IMG.fromarray(maps_img).save(os.path.join(self.log_dir, img_obj.file_name.split('.')[0] + '.png'))
+
+                print(img_obj.file_name + ' PRF1A: ', all_score.get_prf1a())
+
+        if mode is 'train':
+            self._save_if_better(force_checkpoint=force_checkpoint, score=all_score.get_prf1a()[2])
